2af4db387de535c5d99d225d1d37b38f34c5f9b2
StanleyKioko <mwangestanley02@gmail.com>
2022-06-02 05:49:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,33 @@
-#!/bin/bash
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: always 0
+ *
+ *     */
+
+
+
+int main(void)
+
+
+
 {
- puts("Programming is like building a multilingual puzzle");
+
+
+
+	  printf("\"with proper grammar, but the outcome is a piece of art,");
+
+
+
+	    return (0);
+
+
+
 }
-return 0;
