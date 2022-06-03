84713a323180a7e6bd0e3651c5f059ec88f825e5
StanleyKioko <mwangestanley02@gmail.com>
2022-06-02 22:59:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 	/**
-	* main - Prints onto screen
+	*main - Prints onto screen
 	*Return: zero
 */
 	int main(void)
 {
 	printf("with proper gramar,but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
