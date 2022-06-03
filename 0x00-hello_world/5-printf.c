facacc1bb190d5c61da4fa151ebe31cfb212b109
StanleyKioko <mwangestanley02@gmail.com>
2022-06-02 23:02:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 	/**
-	*main - Prints onto screen
+	* main - Prints onto screen
 	*Return: zero
 */
 	int main(void)
 {
-	printf("with proper gramar,but the outcome is a piece of art,\n");
 
-	return (0);
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
