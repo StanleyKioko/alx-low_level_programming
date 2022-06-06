ae5c3cb869632a55b3dc9885e0acba50060dee13
StanleyKioko <mwangestanley02@gmail.com>
2022-06-06 04:14:41 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,11 @@
 	* main - print alphabet in upper and lower
 	* Return: zero
 	*/
-	
-	int main(void)
+int main(void)
 {
 	char ch;
 	char CH;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
 	for (CH = 'A'; CH <= 'Z'; CH++)
