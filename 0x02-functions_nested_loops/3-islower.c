6bd6ae3047a012d09d49e2a2f27781e0696b1ed7
StanleyKioko <mwangestanley02@gmail.com>
2022-06-08 03:32:08 -0700

    3.c git commit -m 3.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-* _islower - checks for lowercase characters.
-* @c: character to be checked
-* Return: Always 0.
-*/
+ * * _islower - Shows 1 if the input is a
+ * * lowercase character. Another cases, shows
+ * * 0
+ * * @c: The character in ASCII code
+ * * Return: 1 for lowercase character. 0 for the rest.
+ * */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
 {
 	return (1);
 }
+	else
+{
 return (0);
-
+}
+	_putchar('\n');
 }
