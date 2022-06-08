baaa90bfcd0b2168296badeb76e66792726727e9
StanleyKioko <mwangestanley02@gmail.com>
2022-06-08 03:05:26 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,29 +1,34 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
+/**
+ *
+ *  * print_alphabet - Prints the alphabet in lowercase.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
 void print_alphabet(void);
 
-void print_alphabet_x10(void);
 
-int _islower(int c);
 
-int _isalpha(int c);
+void print_alphabet(void)
 
-int print_sign(int n);
+{
 
-int _abs(int);
+		int i;
 
-int print_last_digit(int);
 
-void jack_bauer(void);
 
-void times_table(void);
+			for (i = 97; i <= 122; i++)
+			{
 
-int add(int, int);
+						_putchar(i);
 
-void print_to_98(int n);
+							}
 
-void print_times_table(int n);
-#endif
+				_putchar('\n');
+
+}
