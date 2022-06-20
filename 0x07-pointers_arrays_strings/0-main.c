70be99ad3ff38197541d6aae57f6f0d8aa0fe587
StarnleyKioko <mwangestanley02@gmail.com>
2022-06-20 04:38:03 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,21 +1,46 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - thos function fills memory with constant byte
- * @s: memory area to fill
- * @b: constant byte to fill
- * @n: bytes of the memory area to fill
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return: memory area to fill
-*/
-
-char *_memset(char *s, char b, unsigned int n)
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int a;
-	
-	for (a= 0; a < n ; a++)
+	unsigned int i;
+
+	i = 0;
+
+	while (i < size)
 	{
-		[a] =b;
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
 	}
-	return (s)
+	printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char buffer[98] = {0x00};
+simple_print_buffer(buffer, 98);
+_memset(buffer, 0x01, 95);
+printf("-------------------------------------------------\n");
+simple_print_buffer(buffer, 98);
+return (0);
 }
