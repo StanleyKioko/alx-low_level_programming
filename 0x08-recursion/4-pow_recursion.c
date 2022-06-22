9f7f9c4b23c0262b598c9789016742a8e60c5a7e
StarnleyKioko <mwangestanley02@gmail.com>
2022-06-22 04:00:40 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x raised to the powe
+ * @x: interger param
+ * @y: integer param
+ * Return: recursion
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (0);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
