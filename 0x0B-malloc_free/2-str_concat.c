f1f9a42711aa5175db437fe408bba442210cdaab
StarnleyKioko <mwangestanley02@gmail.com>
2022-06-28 01:23:05 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,58 +1,37 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stddef.h>
-
 /**
- * *str_concat - concatenates two strings.
- *
- * @s1: first string.
- * @s2: second string.
- * Return: Always 0.
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer of an array of chars
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, n;
-	char *new_str;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	n = string_length(s1) + string_length(s2) + 1;
-	new_str = malloc(sizeof(char) * n);
-	/* check if null is passed */
-	if (new_str == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
 		return (NULL);
-	/* insert s1 into the new string */
-	while (s1[i] != '\0')
-	{
-		new_str[j] = s1[i];
-		i++;
-		j++;
-	}
-	/* insert s2 into the new string */
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		new_str[j] = s2[i];
-		i++;
-		j++;
-	}
-	new_str[j] = '\0';
-	return (new_str);
-}
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
 
-	while (*(pointer + c) != '\0')
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		c++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+			j++;
+		}
 	}
-	return (c);
+	s3[i] = '\0';
+	return (s3);
 }
