8f20c634c4e5cdfcd81c95e217305732ed448b2d
Stanleykioko <mwangestanley022gmail.com>
2022-07-04 09:15:41 -0700

    SUM

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,24 +1,12 @@
-/*
- * File: 2-main.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file of the program
- *        was compiled from, followed by a new line.
+ * main - prints name of the file it was compled from.
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privac
